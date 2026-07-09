bb6ccda68a3518cff812f14a277b441e0361ee69
Broden Brett <brodenbrett@boxclever.ca>
2026-07-09 16:40:57 -0600

    Module setup and configuration

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Carbon\CarbonImmutable;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Date;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
@@ -23,7 +25,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        $this->configureModels();
         $this->configureDefaults();
+        $this->configureFactoryNameResolvers();
     }
 
     /**
@@ -46,5 +50,52 @@ class AppServiceProvider extends ServiceProvider
                 ->uncompromised()
             : null,
         );
+    }
+
+    /**
+     * Configures Eloquent Models to:
+     *
+     * - Throw an Exception when accessing missing attributes
+     * - Throw an Exception when filling missing attributes
+     * - Detect queries within loops and generate a single query
+     */
+    protected function configureModels(): void
+    {
+        Model::shouldBeStrict();
+        Model::automaticallyEagerLoadRelationships();
+    }
+
+    /**
+     * Extend the factory name resolvers to account for module namespaces.
+     *
+     * For example:
+     * - `App\Models\Foo` ⇄ `Database\Factories\FooFactory`
+     * - `Modules\Foo\Models\Bar` ⇄ `Modules\Foo\Database\Factories\BarFactory`
+     */
+    protected function configureFactoryNameResolvers(): void
+    {
+        Factory::guessModelNamesUsing(function ($factory) {
+            preg_match('/^Modules\\\\([^\\\\]+)\\\\Database\\\\Factories\\\\(.+)$/', $factory::class, $matches);
+            $moduleName = $matches[1] ?? false;
+            $modelBasename = Str::replaceLast('Factory', '', $matches[2] ?? class_basename($factory::class));
+
+            if ($moduleName) {
+                return 'Modules\\'.$moduleName.'\\Models\\'.$modelBasename;
+            }
+
+            return 'App\\Models\\'.$modelBasename;
+        });
+
+        Factory::guessFactoryNamesUsing(function ($modelName) {
+            preg_match('/^Modules\\\\([^\\\\]+)\\\\Models\\\\(.+)$/', $modelName, $matches);
+            $moduleName = $matches[1] ?? false;
+            $modelBasename = $matches[2] ?? class_basename($modelName);
+
+            if ($moduleName) {
+                return 'Modules\\'.$moduleName.'\\Database\\Factories\\'.$modelBasename.'Factory';
+            }
+
+            return 'Database\\Factories\\'.$modelBasename.'Factory';
+        });
     }
 }
