37693e3ab9cccc7723c53638709e3ff28a558ae9
Broden Brett <brodenbrett@boxclever.ca>
2026-07-10 13:18:25 -0600

    eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,12 @@
 import stylistic from '@stylistic/eslint-plugin';
 import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript';
-import prettier from 'eslint-config-prettier/flat';
 import importPlugin from 'eslint-plugin-import';
+import { jsdoc } from 'eslint-plugin-jsdoc';
+import unusedImports from 'eslint-plugin-unused-imports';
 import vue from 'eslint-plugin-vue';
 
 const controlStatements = [
+    'multiline-block-like',
     'if',
     'return',
     'for',
@@ -22,7 +24,21 @@ const paddingAroundControl = [
 ];
 
 export default defineConfigWithVueTs(
-    vue.configs['flat/essential'],
+    {
+        ignores: [
+            'vendor',
+            'node_modules',
+            'public',
+            'bootstrap/ssr',
+            'tailwind.config.js',
+            'vite.config.ts',
+            'resources/js/actions/**',
+            'resources/js/components/ui/*',
+            'resources/js/routes/**',
+            'resources/js/wayfinder/**',
+        ],
+    },
+
     vueTsConfigs.recommended,
     {
         plugins: {
@@ -70,30 +86,124 @@ export default defineConfigWithVueTs(
                 'error',
                 ...paddingAroundControl,
             ],
+            'curly': ['error', 'all'],
+            '@stylistic/indent': ['warn', 4, {
+                ignoreComments: true,
+            }],
+            '@stylistic/semi': ['error', 'always'],
         },
     },
-    {
-        ignores: [
-            'vendor',
-            'node_modules',
-            'public',
-            'bootstrap/ssr',
-            'tailwind.config.js',
-            'vite.config.ts',
-            'resources/js/actions/**',
-            'resources/js/components/ui/*',
-            'resources/js/routes/**',
-            'resources/js/wayfinder/**',
-        ],
-    },
-    prettier,
+
+    // JSDoc
+    jsdoc({
+        config: 'flat/recommended',
+        rules:
+        {
+            'jsdoc/require-jsdoc': ['off'],
+            'jsdoc/no-blank-blocks': ['error'],
+            'jsdoc/require-description-complete-sentence': ['error'],
+            'jsdoc/require-param-description': ['off'],
+            'jsdoc/require-returns-description': ['off'],
+            'jsdoc/require-param': ['off'],
+            'jsdoc/require-returns': ['off'],
+            'jsdoc/require-param-type': ['error'],
+        },
+    }),
+
+    // Unused imports
     {
         plugins: {
-            '@stylistic': stylistic,
+            'unused-imports': unusedImports,
         },
         rules: {
-            curly: ['error', 'all'],
-            '@stylistic/brace-style': ['error', '1tbs', { allowSingleLine: false }],
+            'no-unused-vars': 'off', // or "@typescript-eslint/no-unused-vars": "off",
+            'unused-imports/no-unused-imports': 'warn',
+            'unused-imports/no-unused-vars': ['warn'],
+        },
+    },
+
+    // Vue
+    ...vue.configs['flat/recommended'],
+    {
+        rules: {
+            '@typescript-eslint/no-unused-vars': 'off',
+            '@typescript-eslint/require-param-type': 'off',
+
+              // allow in order to escape HTML entities
+            'vue/no-v-text-v-html-on-component': 'off',
+            'vue/no-v-html': 'off',
+
+            'vue/block-lang': 'off',
+            'vue/multi-word-component-names': 'off',
+            'vue/require-default-prop': 'off',
+            'vue/singleline-html-element-content-newline': 'off',
+            'vue/html-closing-bracket-newline': ['warn', {
+                singleline: 'never',
+                multiline: 'never',
+                selfClosingTag: {
+                    singleline: 'never',
+                    multiline: 'never',
+                },
+            }],
+
+            'vue/max-attributes-per-line': ['warn', {
+                singleline: { max: 4 },
+                multiline: { max: 1 },
+            }],
+
+            'vue/html-self-closing': ['warn', {
+                html: {
+                    void: 'always',
+                    normal: 'any',
+                    component: 'always',
+                },
+                svg: 'always',
+                math: 'always',
+            }],
+
+            'vue/first-attribute-linebreak': [
+                'error',
+                {
+                    singleline: 'beside',
+                    multiline: 'beside',
+                },
+            ],
+            'vue/padding-line-between-tags': ['warn', [
+                {
+                    blankLine: 'always',
+                    prev: '*:single-line',
+                    next: '*:multi-line',
+                },
+                {
+                    blankLine: 'always',
+                    prev: '*:multi-line',
+                    next: '*',
+                },
+            ]],
+
+            'vue/html-indent': [
+                'error',
+                4,
+                {
+                    attribute: 1,
+                    baseIndent: 1,
+                    closeBracket: 1,
+                    alignAttributesVertically: true,
+                    ignores: [],
+                },
+            ],
+            'vue/multiline-html-element-content-newline': ['error', {
+                ignoreWhenEmpty: true,
+                allowEmptyLines: false,
+            }],
+            'vue/mustache-interpolation-spacing': ['warn', 'always'],
+
+            'vue/no-reserved-component-names': ['error', {
+                disallowVueBuiltInComponents: false,
+                disallowVue3BuiltInComponents: false,
+                htmlElementCaseSensitive: true,
+            }],
+
         },
     },
 );
