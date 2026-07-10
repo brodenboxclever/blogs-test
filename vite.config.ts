98197acd512f737dbd53f9c60cdf251bc0552938
Broden Brett <brodenbrett@boxclever.ca>
2026-07-10 14:15:43 -0600

    Vite watch files for changes then execute command

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,46 @@ import { wayfinder } from '@laravel/vite-plugin-wayfinder';
 import tailwindcss from '@tailwindcss/vite';
 import vue from '@vitejs/plugin-vue';
 import laravel from 'laravel-vite-plugin';
+import { exec } from 'child_process';
 import { bunny } from 'laravel-vite-plugin/fonts';
 import { defineConfig } from 'vite';
+
+const execOnChange = (name, command, filesToWatch) => ({
+    name,
+    configureServer(server) {
+        const log = (msg: any) => console.log('\x1b[35m%s\x1b[0m', `[${name}]`, msg);
+
+        const generate = () => {
+            exec(
+                command,
+                (error, stdout, stderr) => {
+                    if (error) console.error(error);
+                    if (stderr) console.error(stderr);
+                    log(stdout);
+                },
+            );
+        };
+
+        generate();
+
+        server.watcher.add(filesToWatch);
+        server.watcher.on('change', (path) => {
+            const matches = filesToWatch.some((file) => {
+                if (file.includes('**')) {
+                    const prefix = file.split('**')[0];
+                    return path.startsWith(prefix);
+                }
+
+                return path === file;
+            });
+
+            if (matches) {
+                log(`Detected change to ${path}`);
+                generate();
+            }
+        });
+    },
+});
 
 export default defineConfig({
     server: {
@@ -41,5 +79,11 @@ export default defineConfig({
         wayfinder({
             formVariants: true,
         }),
+
+        // Custom plugin automatically keeps ziggy in sync with changes to routes
+        execOnChange('wayfinder:generate', 'php artisan wayfinder:generate', [
+            'routes/**/*.php',
+            'modules/**/routes/**/*.php',
+        ])
     ],
 });
