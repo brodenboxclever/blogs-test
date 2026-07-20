35cd541c1da4cc89487938722735a3b77babe05a
Broden Brett <brodenbrett@boxclever.ca>
2026-07-20 16:11:26 -0600

    Add recursion to Pages model

--- modules/Pages/app/Models/Page.php
+++ modules/Pages/app/Models/Page.php
@@ -2,18 +2,38 @@
 
 namespace Modules\Pages\Models;
 
+use App\Concerns\Traits\Models\HasNonPrimaryUuid;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Prunable;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Modules\Pages\Database\Factories\PageFactory;
+use Staudenmeir\LaravelAdjacencyList\Eloquent\HasRecursiveRelationships;
 
 class Page extends Model
 {
     /** @use HasFactory<PageFactory> */
     use HasFactory;
 
+    use HasNonPrimaryUuid;
+    use HasRecursiveRelationships;
     use Prunable, SoftDeletes;
+
+    public function getPathName()
+    {
+        return '_path';
+    }
+
+    public function getCustomPaths()
+    {
+        return [
+            [
+                'name' => 'path',
+                'column' => 'slug',
+                'separator' => '/',
+            ],
+        ];
+    }
 
     /**
      * Get the prunable model query.
